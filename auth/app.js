fe5ceddf21fe44f949bf5cb4a4f9c4f58b7cc786
baderfall <delete-863@yandex.by>
2019-12-19 16:46:35 +0300

    store number of views from every session in closure

--- auth/app.js
+++ auth/app.js
@@ -1,8 +1,33 @@
 const Koa = require('koa');
+const session = require('koa-session');
+
 const app = new Koa();
 
-app.use(async ctx => {
-  ctx.body = 'This should be available only after auth.';
+app.keys = ['secret-key'];
+
+const sessionStorage = {}
+const sessionStorageApi = {
+  set: async (key, data) => {
+    sessionStorage[key] = data
+  },
+  get: async (key) => {
+    return sessionStorage[key]
+  },
+  destroy: async (key) => {
+    delete sessionStorage[key]
+  }
+}
+
+app.use(session(app, { store: sessionStorageApi }));
+
+app.use(ctx => {
+  // ignore favicon
+  if (ctx.path === '/favicon.ico') return;
+
+  let n = ctx.session.views || 0;
+  ctx.session.views = ++n;
+  ctx.body = n + ' views';
 });
 
 app.listen(3000);
+console.log('listening on port 3000');
